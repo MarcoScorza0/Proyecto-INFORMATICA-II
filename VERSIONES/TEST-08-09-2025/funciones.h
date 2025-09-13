141f7fc44c11e1acc86595c1f0ccccad3ede31bd
MarcoScorza0 <marcoscorza0@gmail.com>
2025-09-13 14:41:15 -0300

    Create funciones.h

---
+++ VERSIONES/TEST-08-09-2025/funciones.h
@@ -0,0 +1,7 @@
+
+
+int estado_espera(float deltaT,float T0);
+int estado_compresor_y_forzadores(float deltaT,float T0);
+int estado_forzadores(float deltaT,float T0);
+int estado_descongelamiento(float deltaT, float T0);
+
